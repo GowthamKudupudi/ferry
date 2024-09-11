585698ee8af40d92bb3d0e7d01d5604a9a4a1323
Necktwi Ozfguah <necktwi@ferryfair.com>
2024-09-12 03:47:45 +0530

    recovery,lowerCaseHeaderKeys

--- WSServer.cpp
+++ WSServer.cpp
@@ -37,7 +37,6 @@
 #include <filesystem>
 #include <sys/stat.h>
 
-
 #include <ferrybase/JPEGImage.h>
 #include <FFJSON.h>
 #include <logger.h>
@@ -74,9 +73,10 @@ static void parseHTTPHeader (const char* uri, size_t len, FFJSON& sessionData) {
    while(uri[i]!='\0'){
       if(uri[i]=='\n'){
          if(uri[i-1]=='\n' || (uri[i-1]=='\r' && uri[i-2]=='\n')) {
-            if ((bool)sessionData["Content-type"] &&
-               strstr((ccp)sessionData["Content-type"],"text"))
+            if ((bool)sessionData["content-type"] &&
+               strstr((ccp)sessionData["content-type"],"text")) {
                sessionData["content"]=string(uri+i+1, len-i+1);
+            }
             return;
          };
          
@@ -99,7 +99,9 @@ static void parseHTTPHeader (const char* uri, size_t len, FFJSON& sessionData) {
             int keySize=j-pairStartPin;
             ++j;
             ++j;
-            sessionData[string(uri+pairStartPin,keySize)]=string(uri+j,k-j);
+            string keyStr(uri+pairStartPin,keySize);
+            strLower(keyStr);
+            sessionData[keyStr]=string(uri+j,k-j);
          }
 line_done:         
          pairStartPin=i+1;
@@ -245,8 +247,8 @@ void tls_ntls_common (
       ccp headers = jsonHeader;
       string bid;
       parseHTTPHeader((ccp)hm->uri.ptr, strlen(hm->uri.ptr), sessionData);
-      if (!sessionData["Host"]) return;
-      subdomain=get_subdomain(sessionData["Host"]);
+      if (!sessionData["host"]) return;
+      subdomain=get_subdomain(sessionData["host"]);
       ffl_notice(FPL_HTTPSERV, "subdomain: %s",subdomain.c_str());
       FFJSON& vhost = (bool)config["virtualWebHosts"][subdomain]?
          config["virtualWebHosts"][subdomain]:config;
@@ -254,11 +256,15 @@ void tls_ntls_common (
       FFJSON& users=vhost["users"];
       if (vhost["rootdir"])
          opts.root_dir=(ccp)vhost["rootdir"];
-      if (sessionData["Cookie"])get_cookies(sessionData["Cookie"], cookie);
-      if (!sessionData["Referer"]) goto nextproto;
-      referer=sessionData["Referer"];
+      if (sessionData["cookie"])get_cookies(sessionData["cookie"], cookie);
+      ffl_notice(FPL_HTTPSERV, "cookie[bid]: %s",(ccp)cookie["bid"]);
+      if (cookie["bid"]) {
+         bid=(ccp)cookie["bid"];
+      }
+      if (!sessionData["referer"]) goto nextproto;
+      referer=sessionData["referer"];
       protolen = strstr(referer,":") - referer;
-      sprintf(proto,"%.*s",protolen,(ccp)sessionData["Referer"]);
+      sprintf(proto,"%.*s",protolen,(ccp)sessionData["referer"]);
      nextproto:
       ffl_debug(FPL_HTTPSERV, "proto: %s",proto);
       ffl_notice(FPL_HTTPSERV, "Serving: %s", opts.root_dir);
@@ -285,7 +291,7 @@ void tls_ntls_common (
                goto done;
             }
             rbs[bid]["ts"]=chrono::high_resolution_clock::now();
-            if(rbs[bid]["user"]){
+            if (rbs[bid]["user"]) {
                FFJSON& user=users[(ccp)rbs[bid]["user"]];
                user["bid"]=bid;
                mg_http_reply(c, 200, headers, "%s",
@@ -302,7 +308,7 @@ void tls_ntls_common (
       } else if (!strcmp(sessionData["path"], "/login")) {
          //login
          ffl_notice(FPL_HTTPSERV, "Login");
-         if (!cookie["bid"] || !rbs[bid=(ccp)cookie["bid"]]) {
+         if (!cookie["bid"] || !rbs[bid]) {
             mg_http_reply(c, 200, headers, "{%Q:%Q}", "error", "nobid");
             goto done;
          }
@@ -331,61 +337,136 @@ void tls_ntls_common (
       } else if (!strcmp(sessionData["path"], "/logout")) {
          //login
          ffl_notice(FPL_HTTPSERV, "Logout");
-         if (!cookie["bid"] || !rbs[bid=(ccp)cookie["bid"]]) {
+         if (!bid.length() || !rbs[bid]) {
             mg_http_reply(c, 200, headers, "{%Q:%Q}", "error", "nobid");
             goto done;
          }
          rbs[bid]["user"]=NULL;
          mg_http_reply(c, 200, headers, "{%Q:%s}", "logout","true");
          rbs.save();
+      } else if (!strcmp(sessionData["path"], "/captcha")) {
+         //login
+         ffl_notice(FPL_HTTPSERV, "captcha");
+         if (!bid.length() || !rbs[bid]) {
+            mg_http_reply(c, 200, headers, "{%Q:%Q}", "error", "nobid");
+            goto done;
+         }
+         string tempPath(string(opts.root_dir)+"/tmp/"+bid+".jpg");
+         string randstr = random_alphnuma_string(7);
+         cap randcap(randstr, tempPath, 7, 288, 68, 40, 80, 48);
+         rbs[bid]["captcha"]=randstr;
+         randcap.save();
+         mg_http_reply(c, 200, headers, "{%Q:%s}", "cap","true");
+         rbs.save();
       } else if (!strcmp(sessionData["path"], "/signup")) {
          //signup
+         bool recovery=false;
          ffl_notice(FPL_HTTPSERV, "Signup");
-         if (!cookie["bid"] || !rbs[(ccp)cookie["bid"]]) {
+         if (!bid.length() || !rbs[(ccp)cookie["bid"]]) {
             mg_http_reply(c, 200, headers, "{%Q:%Q}", "error", "nobid");
             goto done;
          }
          FFJSON body(string(hm->body.ptr, hm->body.len));
-         username=body["username"];password=body["password"];
-         ffl_debug(FPL_HTTPSERV, "User: %s\nPass: %s\nEmail: %s",
-                   username, password, (ccp)body["email"]);
+         if (body["username"]) {
+            username=body["username"];
+            ffl_debug(FPL_HTTPSERV, "User: %s\nPass: %s\nEmail: %s",
+                      username, password, (ccp)body["email"]);
+         } else if (body["email"]) {
+            recovery=true;
+            std::map<string,FFJSON*>* emln = vhost["users"].val.pairs;
+            if (emln->find(string((ccp)body["email"]))!=emln->end()) {
+               FFJSON* ffemln = (*emln)[string((ccp)body["email"])];
+               FFJSON::Link* link =
+                  ffemln->getFeaturedMember(FFJSON::FM_LINK).link;
+            username=(*link)[0].c_str();
+            ffl_debug(FPL_HTTPSERV, "username: %s", username);
+            } else {
+               ffl_warn(FPL_HTTPSERV, "%s Email not registered.",
+                        (ccp)body["email"]);
+               mg_http_reply(c, 200, headers, "{%Q:%d,%Q:%Q}", "actEmailSent",
+                          -5, "msg",
+                          "Email not registered!");
+               goto done;
+            }
+         }
+         password=body["password"];
          FFJSON& user=vhost["users"][username];
-         if (user && !user["inactive"]) {
+         if (!recovery && user &&
+             (user["activationKey"] && !user["inactive"])) {
             ffl_warn(FPL_HTTPSERV, "User already exists.");
-            mg_http_reply(c, 200, headers, "{%Q:%d}", "actEmailSent", 0);
+            mg_http_reply(c, 200, headers, "{%Q:%d,%Q:%Q}", "actEmailSent",
+                          -1, "msg",
+                          "Username already taken, choose an another :|");
             goto done;
-         } else if (user["inactive"] && strcmp(body["email"],user["email"])) {
-            ffl_warn(FPL_HTTPSERV, "User already exists.");
-            mg_http_reply(c, 200, headers, "{%Q:%d}", "actEmailSent", 0);
+         } else if (!recovery && user["inactive"] &&
+                    strcmp(body["email"],user["email"])) {
+            ffl_warn(FPL_HTTPSERV, "User exists; mail mismatch");
+            mg_http_reply(c, 200, headers, "{%Q:%d,%Q:%Q}", "actEmailSent",
+                          -2, "msg",
+                          "Username already taken, choose an another :|");
             goto done;
          } else if (
-            user["password"] &&
+            !recovery && user["password"] &&
             users[(ccp)body["email"]].val.fptr!=&users[username]
          ) {
             ffl_warn(FPL_HTTPSERV, "Email already registered.");
-            mg_http_reply(c, 200, headers, "{%Q:%d}", "actEmailSent", 0);
+            mg_http_reply(c, 200, headers, "{%Q:%d,%Q:%Q}", "actEmailSent",
+                          -3, "msg",
+                          "Email already registered! Try resetting password");
+            goto done;
+         } else if (!recovery &&
+                    !(validUsername(string(username)) && strlen(username)>32)
+         ) {
+            ffl_warn(FPL_HTTPSERV, "Invalid password");
+            mg_http_reply(c, 200, headers, "{%Q:%d,%Q:%Q}", "actEmailSent",
+                          -6, "msg", "Invalid password X|");
+            goto done;
+         } else if (
+            !(password!=nullptr && validMD5(string(password)))
+         ) {
+            ffl_warn(FPL_HTTPSERV, "Invalid password");
+            mg_http_reply(c, 200, headers, "{%Q:%d,%Q:%Q}", "actEmailSent",
+                          -6, "msg", "Invalid password X|");
+            goto done;
+         } else if (!body["captcha"] || !rbs[bid]["captcha"] ||
+                    strcmp((ccp)body["captcha"],(ccp)rbs[bid]["captcha"])!=0
+         ) {
+            ffl_warn(FPL_HTTPSERV, "Captcha mismatch.");
+            mg_http_reply(c, 200, headers, "{%Q:%d,%Q:%Q}", "actEmailSent",
+                          -4, "msg", "Captcha mismatch, hmm!");
+            goto done;
+         } else if (!body["consent"]) {
+            ffl_warn(FPL_HTTPSERV, "Captcha mismatch.");
+            mg_http_reply(c, 200, headers, "{%Q:%d,%Q:%Q}", "actEmailSent",
+                          -5, "msg",
+                          "U didn't consent to this tool usage :/");
             goto done;
          }
-         user["password"] = password;
+         if (!recovery) {
          user["email"] = body["email"];
+         user["password"] = password;
          users[(ccp)user["email"]].addLink(&users[username],username);
          user["inactive"]=true;
          filesystem::path
             usrpth(string(opts.root_dir)+string("/upload/")+username);
          filesystem::create_directory(usrpth);
+         } else {
+            user["newpassword"] = password;
+         }
          string actKey = random_alphnuma_string();
          user["activationKey"]=actKey;
          ffl_notice(FPL_HTTPSERV, "actKey: %s",actKey.c_str());
          to=user["email"];
          sprintf(subj, "User activation link");
          sprintf(mesg, "Open %s://%s/activate?user=%s&key=%s to activate "
-                 "%s", proto, (ccp)sessionData["Host"], username,
+                 "%s", proto, (ccp)sessionData["host"], username,
                  (ccp)user["activationKey"], username);
          mg_connect(&mail_mgr, mail_server, mailfn, NULL);
          while(!s_quit)
             mg_mgr_poll(&mail_mgr, 100);
          s_quit=false;
-         mg_http_reply(c, 200, headers, "{%Q:%d}", "actEmailSent", 2);
+         mg_http_reply(c, 200, headers, "{%Q:%d,%Q:%Q}", "actEmailSent", 2,
+                       "msg", "Activation mail sent to ur email :D");
          rbs.save();
          users.save();
       } else if(strstr((ccp)sessionData["path"], "/activate?")){
@@ -393,22 +474,32 @@ void tls_ntls_common (
          get_data_in_url(sessionData["path"], data);
          username=data["user"];
          FFJSON& user=users[username];
-         if (!user["password"] || !user["inactive"]) {
+         if ((!user["password"] || !user["inactive"]) &&
+             !user["newpassword"]) {
             mg_http_reply(c, 400, headers, "{%Q:%Q}", "error", "wrongKey" );
          } else if (!strcmp(user["activationKey"],data["key"])) {
+            if (user["newpassword"]) {
+               user["password"]=user["newpassword"];
+               user["newpassword"]=false;
+            } else {
+               user["name"]=username;
+            }
             user["inactive"]=false;
-            user["name"]=username;
             mg_http_reply(c, 200, headers, "%s activated.", username);
             users.save();
          } else {
             mg_http_reply(c, 400, headers, "{%Q:%Q}", "error", "wrongKey" );
          }
       } else if (strstr((ccp)sessionData["path"], "/update")) {
-         if (!cookie["bid"] || !rbs[bid=(ccp)cookie["bid"]]) {
+         if (!bid.length() || !rbs[bid]) {
             mg_http_reply(c, 400, headers, "{%Q:%Q}", "error", "nobid");
             goto done;
          }
          username=(ccp)rbs[bid]["user"];
+         if (!sessionData["content"]) {
+            mg_http_reply(c, 400, headers, "{%Q:%Q}", "error", "No Content!");
+            goto done;
+         }
          FFJSON cntnt((ccp)sessionData["content"]);
          FFJSON& user=vhost["users"][username];
          if (cntnt["things"]){
@@ -436,7 +527,7 @@ void tls_ntls_common (
          users.save();
       } else if (strstr((ccp)sessionData["path"], "/upload?")){
          //upload?
-         if (!cookie["bid"] || !rbs[bid=(ccp)cookie["bid"]]) {
+         if (!bid.length() || !rbs[bid]) {
             mg_http_reply(c, 400, headers, "{%Q:%Q}", "error", "nobid");
             goto done;
          }
@@ -530,23 +621,20 @@ void tls_ntls_common (
             }
          }
          char msg[30];
-         sprintf(msg, "{\"thingId\":%d,\"picId\":%d,", thingId,picId);
+         sprintf(msg, "{\"thingId\":%d,\"picId\":%d", thingId,picId);
          mg_http_upload(
             c, hm, &mg_fs_posix, upldpth.c_str(), 2999999, msg);
-         if (fofst == ttlSz) {
+         if (fofst+chnkSz >= ttlSz) {
             user.erase("pendingThings");
             user["things"][thngi]["pics"][picId]["partial"] = false;
             printf("pendingThings\n");
             users.save();
-            printf("save\n");
-            
-         }
-         if (fofst==2103296) {
-            printf("its gonna segfault\n");
          }
       } else {
-         if(strstr((ccp)sessionData["path"], "/upload")){
-            if (!cookie["bid"] || !rbs[bid=(ccp)cookie["bid"]]) {
+         if (strstr((ccp)sessionData["path"], "/upload") ||
+            strstr((ccp)sessionData["path"], "/tmp")
+         ) {
+            if (!bid.length() || !rbs[bid]) {
                mg_http_reply(c, 400, headers, "{%Q:%Q}", "error", "nobid");
                goto done;
             }
@@ -594,8 +682,6 @@ WSServer::WSServer (
    #endif
    int iSysLogOptions
 ) {
-   cap c("gowtham", "captcha.jpg", 7, 256, 64, 32, 80, 48);
-   c.save();
    mg_mgr_init(&mgr);
    mg_mgr_init(&mail_mgr);
    char httpsport[16];
